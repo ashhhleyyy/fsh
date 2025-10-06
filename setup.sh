020c5490a5c99d98f7e39f7b80422e249115d662
Mia Moir <me@archess.mn>
2025-10-06 17:19:25 +0100

    fix: Ensure everything else is escaped properly

--- setup.sh
+++ setup.sh
@@ -26,7 +26,7 @@ end" > $HOME/.config/fish/functions/fish_prompt.fish
         echo "Creating $HOME/.config/fish/functions/fish_prompt.fish"
         echo "function fish_prompt
     set FSH_LAST_STATUS \$status
-    if test "$SSH_CLIENT" != "" || test "$SSH_TTY" != "" | test "$SSH_CONNECTION" != ""
+    if test \"\$SSH_CLIENT\" != \"\" || test \"\$SSH_TTY\" != \"\" | test \"\$SSH_CONNECTION\" != \"\"
         set --erase FSH_NO_HOSTNAME
     else
         set -x FSH_NO_HOSTNAME 1
